a5e13376f5e980588e411961d9669836445e139a
esantiagor02 <esantiagor02@gmail.com>
2023-04-14 15:54:55 -0500

    Plantilla inicial página web

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnMenu= document.getElementById('btn-menu');
+let nav= document.getElementById('nav');
+
+btnMenu.addEventListener('click', function(){
+    nav.classList.toggle("mostrar");
+})
