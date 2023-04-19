2026dc9c5fd63457e535fe8861e33f10543b668a
esantiagor02 <esantiagor02@gmail.com>
2023-04-19 11:02:17 -0500

    Integración Header con el routing

--- ESS/src/app/app.module.ts
+++ ESS/src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HEADERComponent } from './HEADER/header/header.component';
+import { InicioComponentComponent } from './app-empresa/inicio-component/inicio-component.component';
+import { AcercadeNosotrosComponent } from './app-AcercadeNosotros/acercade-nosotros/acercade-nosotros.component';
+import { ProductosyServiciosComponent } from './app-Productos/productosy-servicios/productosy-servicios.component';
+import { ContactoComponent } from './app-Contacto/contacto/contacto.component';
+import { RegistroComponent } from './app-Registro/registro/registro.component';
+import { CarritodeCompraComponent } from './app-CarritodeCompra/carritode-compra/carritode-compra.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes:Routes=[
+{path:'',component:InicioComponentComponent},
+{path:'Acerca de nosotros',component:AcercadeNosotrosComponent},
+{path:'Productos y servicios',component:ProductosyServiciosComponent},
+{path:'Contacto',component:ContactoComponent},
+{path:'Registro',component:RegistroComponent},
+{path:'Carrito de compras',component:CarritodeCompraComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HEADERComponent
+    HEADERComponent,
+    InicioComponentComponent,
+    AcercadeNosotrosComponent,
+    ProductosyServiciosComponent,
+    ContactoComponent,
+    RegistroComponent,
+    CarritodeCompraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
