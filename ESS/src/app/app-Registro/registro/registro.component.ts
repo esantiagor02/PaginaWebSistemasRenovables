376af6640b3a52eeedd4c38df3ce3cd0309af1ec
Dario Jose Arteaga <dariojose.arteagavillalb@egmail.com>
2023-05-31 20:48:03 -0500

    se agregan el codigo para renderizar crear usuario

--- ESS/src/app/app-Registro/registro/registro.component.ts
+++ ESS/src/app/app-Registro/registro/registro.component.ts
@@ -2,20 +2,43 @@ import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-
-
-
 export class RegistroComponent {
+  nombre?: string;
+  email?: string;
+  password?: string;
 
-  constructor(){
-  
+  constructor(private http: HttpClient) {}
+
+  enviarDatos() {
+    const datos = {
+      nombre: this.nombre,
+      email: this.email,
+      password: this.password
+    };
+
+    this.enviarAPI(datos);
   }
- 
 
- 
+  enviarAPI(datos: any) {
+    this.http.post('URL_DE_TU_API', datos)
+      .subscribe(
+        response => {
+          // Manejar la respuesta de la API
+          console.log(response);
+        },
+        error => {
+          // Manejar el error en caso de que ocurra
+          console.error(error);
+        }
+      );
+  }
 }
+
